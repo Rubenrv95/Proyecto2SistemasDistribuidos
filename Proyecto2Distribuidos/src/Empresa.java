83fae8383f5bf25564ae04d741aa9287e186ba1b
Luciano <lnicolas15@alumnos.utalca>
2020-11-17 19:54:16 -0300

    Clases creadas
    ideas iniciales

---
+++ Proyecto2Distribuidos/src/Empresa.java
@@ -0,0 +1,51 @@
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Luciano
+ */
+public class Empresa {
+
+    static final String HOST = "localhost";
+    static final int PUERTO=5000;
+    
+    public Empresa () {
+        try{
+            Socket skCliente = new Socket(HOST, PUERTO);
+            InputStream aux = skCliente.getInputStream();
+            DataInputStream flujo = new DataInputStream( aux );
+            DataOutputStream dOut = new DataOutputStream(skCliente.getOutputStream());
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Ingrese operacion [operador x y]: ");
+            String mensaje = reader.readLine();
+
+            dOut.writeUTF(mensaje);
+            System.out.println( flujo.readUTF() );
+            skCliente.close();
+        } catch(Exception e ) {
+            System.out.println( e.getMessage() );
+        }
+    }
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Proyecto2Distribuidos/src/Servicentro.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Luciano
+ */
+public class Servicentro {
+    
+    int valor93;
+    int valor95;
+    int valor97;
+    int valorDiesel;
+    int valorKerosene;
+    
+    public Servicentro()
+    {
+    
+    }
+    
+    public void generarReporte()
+    {
+        
+    }
+    
+    public void calcularFactorUtilidad()
+    {
+        
+    }
+    
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
